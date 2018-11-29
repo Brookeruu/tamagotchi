681396810f3102a6c7ef40c007ac97f29b6f8f60
Brooke Perry <perrysbrooke@gmail.com>
2018-11-28 22:51:40 -0800

    add spec to test rest method, success

--- spec/tamagotchi_spec.rb
+++ spec/tamagotchi_spec.rb
@@ -14,7 +14,7 @@ describe(Tamagotchi) do
   end
 
   describe("#fun") do
-    it("increase activity level") do
+    it("increase activity level but lower energy") do
       my_pet = Tamagotchi.new("lil dragon")
       expect(my_pet.fun()).to(eq("fun"))
       expect(my_pet.activity()).to(eq(10))
@@ -23,11 +23,21 @@ describe(Tamagotchi) do
   end
 
   describe("#feed") do
-    it("increase activity level") do
+    it("increase food and energy leves") do
       my_pet = Tamagotchi.new("lil dragon")
       expect(my_pet.feed()).to(eq("nom!"))
       expect(my_pet.food()).to(eq(25))
       expect(my_pet.energy()).to(eq(25))
     end
   end
+
+    describe("#rest") do
+      it("increase rest level but lower food and activity") do
+        my_pet = Tamagotchi.new("lil dragon")
+        expect(my_pet.rest()).to(eq("yawn!"))
+        expect(my_pet.food()).to(eq(5))
+        expect(my_pet.energy()).to(eq(25))
+        expect(my_pet.activity()).to(eq(-5))
+      end
+    end
 end
