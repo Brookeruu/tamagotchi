99f5d7af235496c7d45ac672e3e3c34e363b7578
Brooke Perry <perrysbrooke@gmail.com>
2018-11-28 23:30:57 -0800

    create basic views files and add  some logic to app.rb

--- app.rb
+++ app.rb
@@ -3,13 +3,13 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/tamagotchi')
 
-describe(Tamagotchi) do
-  describe("#initialize") do
-    it("sets the name and life levels of a new Tamagotchi") do
-      my_pet = Tamagotchi.new("lil dragon")
-      expect(my_pet.name()).to(eq("lil dragon"))
-      expect(my_pet.food()).to(eq(10))
-      expect(my_pet.sleepy()).to(eq(0))
-      expect(my_pet.activity()).to(eq(10))
-    end
-  end
+get('/') do
+  # @pet = Tamagotchi.new()
+  erb(:input)
+end
+
+post('/') do
+  @name = params.fetch("name")
+  new_pet = Tamagotchi.new(name)
+  erb(:output)
+end
